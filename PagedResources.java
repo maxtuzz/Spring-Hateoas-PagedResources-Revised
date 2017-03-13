9272c7d057e7bc42f9746d1a723a451339028a1e
maxtuzz <maxtuzzolino@gmail.com>
2017-03-14 11:33:22 +1300

    Remove package

--- PagedResources.java
+++ PagedResources.java
@@ -1,5 +1,3 @@
-package social.noodle.domain.resources;
-
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
